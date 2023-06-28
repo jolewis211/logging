d696ace5174fa09e89c90c9c62ef8cc1702c99db
Jory <jolewis211@gmail.com>
2023-06-28 16:33:05 -0600

    added datadog boilerplate code

---
+++ datadoglogging.py
@@ -0,0 +1,92 @@
+try:
+    import os
+    import logging
+    from logging import StreamHandler
+    from datadog_api_client.v2 import ApiClient, ApiException, Configuration
+    from datadog_api_client.v2.api import logs_api
+    from datadog_api_client.v2.models import *
+    from faker import Faker
+except Exception as e:
+    print("Error : {} ".format(e))
+
+
+class DDHandler(StreamHandler):
+    def __init__(self, configuration, service_name, ddsource):
+        StreamHandler.__init__(self)
+        self.configuration = configuration
+        self.service_name = service_name
+        self.ddsource = ddsource
+
+    def emit(self, record):
+        msg = self.format(record)
+
+        with ApiClient(self.configuration) as api_client:
+            api_instance = logs_api.LogsApi(api_client)
+            body = HTTPLog(
+                [
+                    HTTPLogItem(
+                        ddsource=self.ddsource,
+                        ddtags="env:{}".format(
+                            os.getenv("ENV"),
+
+                        ),
+                        message=msg,
+                        service=self.service_name,
+                    ),
+                ]
+            )
+
+            try:
+                # Send logs
+                api_response = api_instance.submit_log(body)
+            except ApiException as e:
+                print("Exception when calling LogsApi->submit_log: %s\n" % e)
+
+
+class Logging(object):
+    def __init__(self, service_name, ddsource, logger_name='demoapp'):
+
+        self.service_name = service_name
+        self.ddsource = ddsource
+        self.logger_name = logger_name
+
+
+        self.configuration = Configuration()
+        format = "[%(asctime)s] %(name)s %(levelname)s %(message)s"
+        self.logger = logging.getLogger(self.logger_name)
+        formatter = logging.Formatter(
+            format,
+        )
+
+
+        # Logs to Datadog
+        dd = DDHandler(self.configuration, service_name=self.service_name,ddsource=self.ddsource)
+        dd.setLevel(logging.INFO)
+        dd.setFormatter(formatter)
+        self.logger.addHandler(dd)
+
+        if logging.getLogger().hasHandlers():
+            logging.getLogger().setLevel(logging.INFO)
+        else:
+            logging.basicConfig(level=logging.INFO)
+
+def main():
+    import time
+    os.environ['DD_API_KEY'] = "XXXXXXXXXXXXXXXXXXXXXXXX"
+    os.environ['DD_SITE'] ="datadoghq.com"
+    os.environ['ENV'] = "DEV"
+
+    logger = Logging(service_name='first-service', ddsource='source1', logger_name='demoapp')
+    for i in range(1, 10):
+        _instance = Faker()
+
+
+        _data = {
+            "first_name": _instance.first_name()
+        }
+        logger.logger.info(_data)
+        logger.logger.error(_data)
+        time.sleep(1)
+
+
+main()
